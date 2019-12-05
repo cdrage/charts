6eee06c2a715fe8c232abd9e9bcf3f85bd59f4a2
Charlie Drage <charlie@charliedrage.com>
2019-12-05 17:03:33 -0500

    Add build script
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+git checkout gh-pages
+
+CURRENT_DIR=$PWD
+DIR=`mktemp -d`
+
+cd $DIR
+
+git clone https://github.com/cdrage/charts && cd charts
+
+for i in */; do
+  helm package --sign --key "Charlie Drage <charlie@charliedrage.com>" --keyring ~/.gnupg/secring.gpg gitea
+done
+
+mv *.tgz *.tgz.prov $CURRENT_DIR
+
+cd $CURRENT_DIR
+
+git add .
+DATE=`date`
+git commit -m "Update $DATE"
+git push origin gh-pages
+
+git checkout master
