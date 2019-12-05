31d768fa915859267f6e117ebdc647b6caec386d
Charlie Drage <charlie@charliedrage.com>
2019-12-05 17:10:16 -0500

    Update script again
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- update.sh
+++ update.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-git checkout gh-pages
-
 CURRENT_DIR=$PWD
 DIR=`mktemp -d`
 
 cd $DIR
 
 git clone https://github.com/cdrage/charts && cd charts
+
+echo "Enter your GPG key:"
+read gpgkey
+
+export HELM_KEY_PASSPHRASE=$gpgkey
 
 for i in */; do
   helm package --sign --key "Charlie Drage <charlie@charliedrage.com>" --keyring ~/.gnupg/secring.gpg gitea
@@ -19,9 +22,9 @@ mv *.tgz *.tgz.prov $CURRENT_DIR
 
 cd $CURRENT_DIR
 
+helm repo index .
+
 git add .
 DATE=`date`
 git commit -m "Update $DATE"
 git push origin gh-pages
-
-git checkout master
