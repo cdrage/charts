7ac8999d986c5aed634fcfa1937eae033f7bcaca
Charlie Drage <charlie@charliedrage.com>
2019-12-05 17:18:19 -0500

    Another update
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- update.sh
+++ update.sh
@@ -9,11 +9,6 @@ cd $DIR
 
 git clone https://github.com/cdrage/charts && cd charts
 
-echo "Enter your GPG key:"
-read gpgkey
-
-export HELM_KEY_PASSPHRASE=$gpgkey
-
 for i in */; do
   helm package --sign --key "Charlie Drage <charlie@charliedrage.com>" --keyring ~/.gnupg/secring.gpg gitea
 done
